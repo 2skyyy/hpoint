51c87820a3658f2dc37599674c997f0bf9f61f0b
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-04-11 20:29:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,15 +11,17 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
-var swiper3 = new Swiper3(".s3_mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    }
-});
+
+
+//side
+
+const hiddenMenu = document.querySelector('.hidden_menu');
+const menu = document.querySelector('.menu');
+const closed = document.querySelector('.close');
+
+menu.onclick = () => {
+    hiddenMenu.classList.add('active');
+}
+closed.onclick = () => {
+    hiddenMenu.classList.remove('active');
+}
