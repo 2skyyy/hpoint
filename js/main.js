604415c8990ef89a04d1caf1fea3788aba7327f3
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-04-11 13:21:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,6 +7,19 @@ var swiper = new Swiper(".mySwiper", {
     },
     pagination: {
     el: ".swiper-pagination",
+    clickable: true
+    }
+});
+
+var swiper3 = new Swiper3(".s3_mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
     clickable: true,
     }
 });
